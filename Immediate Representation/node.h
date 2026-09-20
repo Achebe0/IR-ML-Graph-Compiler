bfcc4b567700cfe440dc67340c54527a04af3acd
Achebe <achebeculpepper@gmail.com>
2026-09-20 12:42:02 -0400

    defined the node structure for JSON parsing

--- Immediate Representation/node.h
+++ Immediate Representation/node.h
@@ -6,3 +6,20 @@
 #define IR_ML_GRAPH_COMPILER_NODE_H
 
 #endif //IR_ML_GRAPH_COMPILER_NODE_H
+#include  <nlohmann/json.hpp>
+
+struct Node {
+    std::string id;
+    std::string op;
+    std::target target;
+    std::nlohmann::json args;
+    std::nlohmann::json kwargs;
+
+};
+
+struct Graph {
+    //format
+    //version
+    //nodes
+    //inputs and outputs
+};
